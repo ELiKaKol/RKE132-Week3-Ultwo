bad87fa77e5b007530ea128e9f99ae90ebcb6ebb
EliKaKol <elisak.kolga@gmail.com>
2024-09-22 18:48:36 +0300

    add ül2week3 code

---
+++ Ül2Week3Switch/Program.cs
@@ -0,0 +1,27 @@
+﻿
+char grade;
+Console.WriteLine("Enter your assessment:");
+string input = Console.ReadLine().ToUpper(); //ToUpper, так как использвуем заглавные букв3
+grade = input[0];
+
+switch (grade) 
+{
+    case 'A': 
+        Console.WriteLine("Suurepärane!");
+        break;
+    case 'B':
+        Console.WriteLine("Väga hea!");
+        break;
+    case 'C':
+        Console.WriteLine("Hea!");
+        break;
+    case 'D':
+        Console.WriteLine("Rahuldav");
+        break;
+    case 'F':
+        Console.WriteLine("Puudulik!");
+        break;
+    default:
+        Console.WriteLine("Vale väärtus");
+        break;
+}
